4ef50c2dcad19a16cb0be5e23268c24614149179
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-03-02 17:04:42 -0500

    updated dependencies. started Authenticate functions.

--- frontend.py
+++ frontend.py
@@ -2,8 +2,8 @@
 #data tools
 import pandas as pd
 import numpy as np
+import qgrid
 from pymatgen.ext.matproj import MPRester
-from pymatgen.core import Structure
 
 #simtool loading and interface
 from simtool import findInstalledSimToolNotebooks,searchForSimTool
@@ -11,14 +11,46 @@ from simtool import getSimToolInputs,getSimToolOutputs,Run
 
 #user interface utilities 
 import os, stat
+import plotly.express as px
 import ipywidgets as widgets
 from IPython.display import display
 from IPython.display import clear_output
 
-#visualization tools
-import qgrid
-import plotly
-#import matplotlib.pyplot as plt
+class Authenticate:
+    """
+    Container class for user authentication functions
+
+    eventually provide an interface to authenticate into multiple data providers
+    - materials project
+    - citrine informatics
+    - nomad?
+
+    for now, just materials project is provided.
+    (current remote query apps work only through MP anyway)
+    (look into OPTIMATE?)
+    """
+    @staticmethod
+    def mpkey():
+        """
+        write user's materials project key to a file which can be
+        accessed by later instances of MPRester. Delete key from
+        kernel vars dict.
+        """
+        try:
+            mpkey = str(input('Paste MP API key: '))
+            clear_output()
+            if not mpkey.isalnum():
+                raise TypeError('Wrong Key')
+            if mpkey is None:
+                raise TypeError('Empty')
+            with open(os.path.expanduser('~/.mpkey.txt'), 'w') as keyfile:
+                keyfile.write(mpkey)
+            del mpkey
+            os.chmod(os.path.expanduser('~/.mpkey.txt'), stat.S_IREAD | stat.S_IWRITE)
+            print("Success")
+        except:
+            print("Something seems wrong with your key")
+
 
 # modification of Hublib's HideCodeButton
 class HideCodeButton(object):

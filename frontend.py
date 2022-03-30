bde0e6d5d75c6bfd3ec4ddb17036a84f4492cf0c
PanayotisManganaris <panos.manganaris@gmail.com>
2022-03-29 23:46:41 -0400

    Appmode is an embarassing simple solution

--- frontend.py
+++ frontend.py
@@ -13,8 +13,7 @@ from simtool import getSimToolInputs,getSimToolOutputs,Run
 import os, stat
 import plotly.express as px
 import ipywidgets as widgets
-from IPython.display import display
-from IPython.display import clear_output
+from IPython.display import display, Javascript, clear_output
 
 class Authenticate:
     """
@@ -221,64 +220,3 @@ class InputSuite():
             inputs['pps'].value = [pp_menu1.value, pp_menu2.value]
             inputs['smearing'].value = smearing.value
             inputs['struct_dict'].value = struct_dict
-
-# modification of Hublib's HideCodeButton
-class HideCodeButton(object):
-
-    def __init__(self, **kwargs):
-
-        label = kwargs.get('label', None)
-        icon = kwargs.get('icon', '')
-        tooltip = kwargs.get('tooltip', '')
-        style = kwargs.get('style', '')
-        bcb = kwargs.get('cb', None)
-        runall = kwargs.get('RunAll', False)
-
-        self.func = 'hide'
-
-        if label is None:
-            self.hlabel = "Hide Code Cells"
-            self.slabel = "Show Code Cells"
-        elif isinstance(label, list):
-            self.hlabel, self.slabel = label
-        elif isinstance(label, str):
-            self.hlabel = self.slabel = label
-        else:
-            raise ValueError('label should be a string or list containing two strings')
-
-        if style is None:
-            self.hstyle = ''
-            self.sstyle = ''
-        elif isinstance(style, list):
-            self.hstyle, self.sstyle = style
-        elif isinstance(style, str):
-            self.hstyle = self.sstyle = style
-        else:
-            raise ValueError('style should be a string or list containing two strings')
-
-        self.w = widgets.Button(
-            description=self.hlabel,
-            icon=icon,
-            tooltip=tooltip,
-            button_style=self.hstyle
-            )
-
-        def button_cb(ignore):
-            js = Javascript("$('div.input').%s()" % self.func)
-            if self.func == 'hide':
-                self.func = 'show'
-                self.w.description = self.slabel
-                self.w.button_style = self.sstyle
-            else:
-                self.func = 'hide'
-                self.w.description = self.hlabel
-                self.w.button_style = self.hstyle
-
-            if bcb is not None:
-                bcb()
-            display(js)
-
-        self.w.on_click(button_cb)
-
-    def _ipython_display_(self):
-        self.w._ipython_display_()

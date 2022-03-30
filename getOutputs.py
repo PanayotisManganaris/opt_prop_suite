970f4b0eb716a801a2e2a381a405d12663a430e5
PanayotisManganaris <panos.manganaris@gmail.com>
2022-03-30 16:15:34 -0400

    test successful new ZnS SSSD pseudopotentials

--- getOutputs.py
+++ getOutputs.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 import numpy as np
 import os
 import glob
@@ -9,10 +9,11 @@ def isadigit(string):
     return bool(re.match(r'[-+]?(?:\d+(?:\.\d*)?|\.\d+)', string))
 
 def makePlot():
-    filepath = "*.vc-relax.out"
+    filepath = os.path.expanduser("/home/panos/src/opt_prop_suite/*.vc-relax.out")
     txt = glob.glob(filepath)
-    for file in txt:
-        output = open(file, "r")
+    print(txt)
+    for file_name in txt:
+        output = open(file_name, "r")
         lines = output.readlines()
         E = []
         for line in lines:
@@ -23,21 +24,30 @@ def makePlot():
 
         output.close()
 
-    # print(E)
+    print(E)
     E = [float(e) for e in E]
     # pd.series(E)
     # print(E)
     # E = [e for e in E if e.isnumeric()]
     s = np.linspace(1, len(E), len(E))
 
-    plt.figure()
-    plt.plot(s,E)
-    plt.xlabel("ionic step")
-    plt.ylabel("energy (Ry)")
-    plt.title("Energy Convergence")
-    plt.savefig("E.png")
-    plt.close()
+    #pick plot that works... f.write_image doesn't work anyway....
+#    plt.figure()
+#    plt.plot(s,E)
+#    plt.xlabel("ionic step")
+#    plt.ylabel("energy (Ry)")
+#    plt.title("Energy Convergence")
+#    plt.savefig("E.png")
+#    plt.close()
 
+    f = go.Figure(go.Scatter(x=s,y=E))
+    f.update_layout(
+        title = "Energy Convergence",
+        xaxis_title = "ionic steps",
+        yaxis_title = "energy (Ry)"
+    )
+    f.write_image("e.png")
+    
 def addInputs(filename, df):
 
     inputs = open("inputs.yaml","r")
@@ -61,15 +71,14 @@ outputs = ['dir','ecutwfc','kpoints','pp1','pp2']
 
 df = pd.DataFrame(columns=outputs)
 
-for filename in os.listdir('RUNS'):
-
-    f = os.path.join('RUNS',filename)
-    if os.path.isdir(f):
-        os.chdir(f)
-        try:
-            makePlot()
-            df = addInputs(filename, df)
-        except:
-            print("failed for "+filename)
-        os.chdir('../..')
+# for filename in os.listdir('RUNS'):
+# 
+#     f = os.path.join('RUNS',filename)
+#     if os.path.isdir(f):
+#         os.chdir(f)
+#         makePlot()
+#         df = addInputs(filename, df)
+#         #except:
+#         #    print("failed for "+filename)
+#         os.chdir('../..')
 df.to_csv("inputs.csv")

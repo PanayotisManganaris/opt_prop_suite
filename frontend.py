95ae1bd4a3a18b3e184c6dd96e901b568025edbb
PanayotisManganaris <panos.manganaris@gmail.com>
2022-04-27 09:33:52 -0400

    querypanel and reviewsuite done

--- frontend.py
+++ frontend.py
@@ -14,42 +14,59 @@ import os, stat
 import plotly.express as px
 import ipywidgets as widgets
 from IPython.display import display, Javascript, clear_output
+from functools import partial
+import time
 
 class Authenticate():
     """
     Container class for user authentication functions
 
-    eventually provide an interface to authenticate into multiple data providers
+    Provides interfaces to authenticate into multiple data providers
     - materials project
-    - citrine informatics
+    - citrine informatics?
     - nomad?
-
-    for now, just materials project is provided.
-    (current remote query apps work only through MP anyway)
-    (look into OPTIMATE?)
     """
     def __init__(self):
+        #AutoAuth Checks
         self.checkmp = os.path.isfile(os.path.expanduser("~/.mpkey.txt"))
+        #Control and Base Widgets
+        self.authbox = widgets.Text(value="", description='Token', disabled=False)
+        self.good = widgets.IntProgress(value=10, min=0, max=10,
+                                        description="You're In!", bar_style="success")
+        self.renew_button = widgets.Button(description='Renew Tokens',
+                                           disabled=False,
+                                           button_style='warning')
+        def forceAuth(event):
+            setattr(self, 'force', True)
+            #time.sleep(1.0)
+            display(Javascript("require(['base/js/namespace'], function(IPython) {IPython.notebook.run_cells();});"))
+            #this does successfully set the force attribute on button press
+            #but, it seems the cell is not properly rerun afterwards,
+            #requiring manual re-run to display auth-suite in force mode..... global variable? inherit force? more JS?
+        self.renew_button.on_click(forceAuth)
+        #Auth widgets
+        self.mpkey = partial(self.token, authmethod=self._mpkey, check=self.checkmp)
 
-    def mpkey(self):
+    def token(self, authmethod, check):
         """
-        ask user for authentication if they do not already have mpkey file
+        generic conditional authentication
         """
-        if self.checkmp:
-            pass
+        if check and not hasattr(self, "force"):
+            return self.good
         else:
-            self._mpkey()
+            self.authbox.on_submit(authmethod)
+            return self.authbox
 
     @staticmethod
-    def _mpkey():
+    def _mpkey(text):
         """
-        write user's materials project key to a file which can be
-        accessed by later instances of MPRester. Delete key from
-        kernel vars dict.
+        mpkey authmethod -- write user's materials project key to a
+        file which can be accessed by later instances of
+        MPRester. Delete key from kernel vars dict.
         """
+        mpkey = text.value
+        text.close() 
         try:
-            mpkey = str(input('Paste MP API key: '))
-            clear_output()
             if not mpkey.isalnum():
                 raise TypeError('Wrong Key')
             if mpkey is None:
@@ -58,19 +75,19 @@ class Authenticate():
                 keyfile.write(mpkey)
             del mpkey
             os.chmod(os.path.expanduser('~/.mpkey.txt'), stat.S_IREAD | stat.S_IWRITE)
-            print("Success")
-        except:
+            print("Key is Viable -- if your queries return authentication errors, use the 'Renew Token' button to try again")
+        except TypeError:
             print("Something seems wrong with your key")
 
 class MPQuery():
-    """ might want to try implementing OPTIMATE """
+    """ The Materials Project Query Object used by this tool """
     def __init__(self, content=[], **kwargs):
         """current stopgap:
         if OPTIMATE is hard to implement, just make case-by-case query
         object with the following API
 
         authentication is pulled from the user's filesystem to offload security onto them...
-        frame returns a dataframe of requested values
+        frame attribute returns a dataframe of requested values
         """
         self.authfile = kwargs.get('authfile', "~/.mpkey.txt")
         self.query = kwargs.get('query', ({ "crystal_system": "cubic"},
@@ -83,7 +100,7 @@ class MPQuery():
     def _make_rester(self):
         with open(os.path.expanduser(self.authfile), "r+") as file:
             apikey = file.readline()
-        return MPRester(apikey) #investigate other apis, plug and play?... make a function probably. OPTIMATE?
+        return MPRester(apikey) #investigate other apis, plug and play parent? OPTIMATE?
 
     def get_content(self, content):
         """mutate list of MP results"""
@@ -96,39 +113,90 @@ class MPQuery():
                                                         final=False,
                                                         conventional_unit_cell=False)
 
-class QuerySuite():
-    """widgets for making and reviewing queries"""
+class FakeQuery():
+    """ A Materials Project Debugging Query Object """
+    def __init__(self, content=[], Debug=False, **kwargs):
+        self.frame = pd.DataFrame(["You must authenticate into a remote data source and run a query to use this interface."],
+                                  index=["alert!"], columns=["attention!"])
+        if Debug:
+            self.frame = pd.read_pickle("./cubics.pkl")
+        monitor = widgets.Output()
+
+    def get_structure(self, mpid):
+        raise NotImplementedError("No rest api needed to see this output!")
+
+class QueryPanel():
+    """pick from a selection of pre-established queries"""
+    def __init__(self):
+        self.Toggles = widgets.ToggleButtons(
+            options=["None",
+                     "Debug", #comment for release
+                     "Materials Project"],
+            description="Remote Data: ",
+            disabled=False,
+            button_style="",
+            tooltips=["load a saved query for quick debugging",
+                      "Contact the Materials Project REST API for semiconductor information"]
+        )
+        self.Q = FakeQuery() #default
+        self.Toggles.observe(self._assign_data_on_pick)
+
+    def _assign_data_on_pick(self, change):
+        """ observes update to the traitlets bunch, doesn't use it... too much throughput"""
+        if self.Toggles.value == "None":
+            self.Q = FakeQuery()
+        elif self.Toggles.value == "Materials Project":
+            self.Q = MPQuery()
+        elif self.Toggles.value == "Debug":
+            self.Q = FakeQuery(Debug=True)
+
+class ReviewSuite():
+    """create and return widgets for making and reviewing queries -- display arrangement handled externally"""
     def __init__(self, QueryObj):
-        q = QueryObj
-        self.menu = qgrid.show_grid(q.frame)
-        
+        self.Q = QueryObj
+        self.plotout = widgets.Output(layout={'border': '5px solid black'})
+        self.menu = qgrid.show_grid(self.Q.frame) #make grid widget with convenience method
+        self.searchbox = widgets.IntText(
+            value=2133,
+            description='ID:',
+            disabled=False
+        )
         self.plotbutton = widgets.Button(
             description='plot',
             disabled=False,
             button_style='', # 'success', 'info', 'warning', 'danger' or ''
             tooltip='plot'
         )
-        def click_to_plot_structure(b):
-            selected_row = self.menu.get_changed_df().index[self.menu.get_selected_rows()][0]
-            mpid_selected = q.frame.at[selected_row,'task_id']
-            struct = q.get_structure(mpid_selected)
-            self.mpid_plot(struct)
+        
+        clickact = partial(self._plot_on_button_click, out=self.plotout,
+                           Q=self.Q, watch_widget=self.searchbox)
+        pickact = partial(self._plot_on_pick_row, out=self.plotout,
+                          Q=self.Q, grid=self.menu)
 
-        self.plotbutton.on_click(click_to_plot_structure)
-    
-    def get_qgrid(self):
-        return self.menu
+        self.plotbutton.on_click(clickact)
+        self.menu.observe(pickact)
+        
+    def _assign_struct_and_plot(self, id):
+        self.struct = self.Q.get_structure(id)
+        self.struct_plot(self.struct)
 
-    def get_plot(self):
-        return self.plotbutton
+    def _plot_on_button_click(self, event, out, Q, watch_widget):
+        """ raw callback for generic id widget """
+        with out:
+            clear_output(wait=True)
+            mpid="mp-"+str(watch_widget.value)
+            self._assign_struct_and_plot(mpid)
 
-    def _display_qgrid(self):
-        """debugging qgrid"""
-        display(self.menu)
-
-
+    def _plot_on_pick_row(self, event, out, Q, grid):
+        """ raw callback for qgrid menu """
+        with out:
+            clear_output(wait=True)
+            mpid = grid.get_changed_df().at[grid.get_selected_rows()[0],'task_id']
+            self._assign_struct_and_plot(mpid)
+            
     @staticmethod
-    def mpid_plot(struct):
+    def struct_plot(struct):
+        """ Plotly 3-D plot function displays the unit cell of a structure object """
         # import POSCAR file
         POSCAR_str = struct.to(fmt = "poscar")
         lines = POSCAR_str.split('\n')
@@ -157,14 +225,18 @@ class QuerySuite():
         colors = []
         for i, atom in enumerate(atoms):
             colors.extend([elements[i]]*int(atoms[i]))
+
         zip_iterator = zip(elements,hues)
         color_dict = dict(zip_iterator)
+
         size = 72
         POSCAR_df = pd.DataFrame({'x':xyz[0],'y':xyz[1],'z':xyz[2],'element':colors,'size':size})    
         fig = px.scatter_3d(POSCAR_df, x='x', y='y', z='z',
                   color='element',labels={'x':'','y':'','z':''},size='size',size_max=size)
+
         box = px.line_3d(box,x='x', y='y', z='z')
         fig.add_traces(list(box.select_traces()))
+
         fig.update_layout(scene = dict(
                         xaxis = dict(
                             nticks=0,showbackground=False,showticklabels=False,),

9f686a83ff652b26af7933133d6e2888bfa12e1d
PanayotisManganaris <panos.manganaris@gmail.com>
2022-05-01 16:19:37 -0400

    struct_plot elevated to frontend namespace. layout filemenu

--- frontend.py
+++ frontend.py
@@ -15,10 +15,10 @@ import os, stat, io
 import plotly.express as px
 import ipywidgets as widgets
 from IPython.display import display, Javascript, clear_output
-from hublib.ui import FileUpload
 
 from functools import partial
 import time
+import codecs
 
 class Authenticate():
     """
@@ -128,6 +128,59 @@ class FakeQuery():
     def get_structure(self, mpid):
         raise NotImplementedError("No rest api needed to see this output!")
 
+def struct_plot(struct):
+    """ 3-D Interactive plot function for inspecting the unit cell of a material """
+    POSCAR_str = struct.to(fmt = "poscar")
+    lines = POSCAR_str.split('\n')
+    # get the lattice information
+    lattice = lines[1]
+    cell_vectors = np.array([lines[2].split() , lines[3].split() , lines[4].split()]).astype(float)
+    # get the list of sites
+    sites = []
+    for line in lines[8:]:
+        if not line:
+            break
+        sites.append([line.split()[0],line.split()[1],line.split()[2]])
+    # convert from fractional to xyz
+    sites = np.array(sites).astype(float)
+    xyz = np.matmul(sites,cell_vectors).transpose()
+    # get the coordinates of the box
+    corners = np.array([[0,1,1,0,0,0,0,1,1,0,0,1,1,1,1,0,0],
+                        [0,0,1,1,0,0,0,0,1,1,0,0,0,1,1,1,1],
+                        [0,0,0,0,0,1,1,1,1,1,1,1,0,0,1,1,0]]).T
+    cell = np.matmul(corners,cell_vectors).transpose()
+    box = pd.DataFrame({'x':cell[0],'y':cell[1],'z':cell[2]})
+    # get a color dictionary
+    elements = lines[5].split()
+    atoms = lines[6].split()
+    hues = ['tab:blue','tab:orange','tab:green','tab:red']
+    colors = []
+    for i, atom in enumerate(atoms):
+        colors.extend([elements[i]]*int(atoms[i]))
+
+    zip_iterator = zip(elements,hues)
+    color_dict = dict(zip_iterator)
+
+    size = 72
+    POSCAR_df = pd.DataFrame({'x':xyz[0],'y':xyz[1],'z':xyz[2],'element':colors,'size':size})    
+    fig = px.scatter_3d(POSCAR_df, x='x', y='y', z='z',
+              color='element',labels={'x':'','y':'','z':''},size='size',size_max=size)
+
+    box = px.line_3d(box,x='x', y='y', z='z')
+    fig.add_traces(list(box.select_traces()))
+
+    fig.update_layout(scene = dict(
+                    xaxis = dict(
+                        nticks=0,showbackground=False,showticklabels=False,),
+                    yaxis = dict(
+                        nticks=0,showbackground=False,showticklabels=False,),
+                    zaxis = dict(
+                        nticks=0,showbackground=False,showticklabels=False,),),
+                    width=700,
+                    margin=dict(r=10, l=10, b=10, t=10)
+                  )
+    fig.show()
+
 class QueryPanel():
     """
     widget class
@@ -162,34 +215,29 @@ class QueryPanel():
             elif self.toggles.value == "Debug":
                 self.Q = FakeQuery(Debug=True)
 
-class StructureSuite():
-    """create and return widgets for making and reviewing queries -- display arrangement handled externally"""
+class InputSuite():
+    """
+    create and return widgets for selecting and reviewing a structure from various sources
+    1. use instance plotout widget to view a selected structure
+    2. use remote_menu to select a structure from an authorized and connected REST API
+    3. use copybox to write or copy/paste a plain text structure acceptable by pymatgen
+    4. use upload_button and file_menu to select from a batch of structure files of potentially mixed types
+    """
     def __init__(self, QueryObj):
         self.Q = QueryObj
-        #inspection widgets
+        # inspection widgets
         self.plotout = widgets.Output(layout={'border': '5px solid black'})
-        #remote interface widget
-        self.menu = qgrid.show_grid(self.Q.frame) #make grid widget with convenience method
-        self.menu.observe(self._process_pick_row)
-        #local interface widgets
-
-        # configure so the uploaded file is written to the copybox for review?
-        # almost certainly will inconvenience any attempts to chain structures for sequential simulation...
-
-        # self.upload_button = FileUpload(name="Upload Structure",
-        #                                 desc="POSCAR, cif, etc..",
-        #                                 dir="./",
-        #                                 maxnum=1,
-        #                                 cb=_read_into_copybox)
-        # def _read_into_copybox(copybox, filename):
-        #     pass
-
-        self.copybox = widgets.Textarea(value="",
-                                        placeholder="formula\nscale\nx y z\na b c\netc... ",
-                                        description="Structure: ",
-                                        disabled=False,
-                                        layout=widgets.Layout(height="300px", width="auto"))
-
+        # remote interface widgets
+        self.remote_menu = qgrid.show_grid(self.Q.frame) #make grid widget with convenience method
+        self.remote_menu.observe(self._process_pick_remote)
+        # write widgets
+        self.copybox = widgets.Textarea(
+            value="",
+            placeholder="formula\nscale\nx y z\nel\na b c\netc... ",
+            description="Structure: ",
+            disabled=False,
+            layout=widgets.Layout(height="300px", width="auto")
+        )
         self.plotbutton = widgets.Button(
             description='Plot',
             disabled=False,
@@ -197,7 +245,15 @@ class StructureSuite():
             tooltip='Generates Structure and Plots'
         )
         self.plotbutton.on_click(self._process_input)
-
+        # file widgets
+        self.upload_button = widgets.FileUpload(
+            accept='',
+            multiple=True,
+        )
+        self.upload_button.value
+        self.file_menu = qgrid.show_grid()
+        self.file_menu.observe(self._process_pick_file)
+    
     def _induce_format(self, raw_string:str):
         """ pymatgen contains no logic for this, so here is a simple stuffer """
         self.struct = Structure.from_str(raw_string,
@@ -206,77 +262,32 @@ class StructureSuite():
                                          fmt="poscar") #currently hardcoded
 
     def _process_input(self, event):
-        """ raw callback for generic id widget """
+        """ callback for copybox """
         with self.plotout:
             clear_output(wait=True)
             self._induce_format(self.copybox.value)
-            self.struct_plot(self.struct)
+            struct_plot(self.struct)
 
-    def _process_pick_row(self, event):
-        """ raw callback for qgrid menu """
-        grid = self.menu
+    def _process_pick_remote(self, event):
+        """ callback for menu """
+        grid = self.remote_menu
         with self.plotout:
             clear_output(wait=True)
             sid = grid.get_changed_df().at[grid.get_selected_rows()[0],'task_id'] #hardcoded for mp. id key should depend on Q
             self.struct = self.Q.get_structure(sid)
-            self.struct_plot(self.struct)
-            
-    @staticmethod
-    def struct_plot(struct):
-        """ Plotly 3-D plot function displays the unit cell of a structure object """
-        # import POSCAR file
-        POSCAR_str = struct.to(fmt = "poscar")
-        lines = POSCAR_str.split('\n')
-        # get the lattice information
-        lattice = lines[1]
-        cell_vectors = np.array([lines[2].split() , lines[3].split() , lines[4].split()]).astype(float)
-        # get the list of sites
-        sites = []
-        for line in lines[8:]:
-            if not line:
-                break
-            sites.append([line.split()[0],line.split()[1],line.split()[2]])
-        # convert from fractional to xyz
-        sites = np.array(sites).astype(float)
-        xyz = np.matmul(sites,cell_vectors).transpose()
-        # get the coordinates of the box
-        corners = np.array([[0,1,1,0,0,0,0,1,1,0,0,1,1,1,1,0,0],
-                            [0,0,1,1,0,0,0,0,1,1,0,0,0,1,1,1,1],
-                            [0,0,0,0,0,1,1,1,1,1,1,1,0,0,1,1,0]]).T
-        cell = np.matmul(corners,cell_vectors).transpose()
-        box = pd.DataFrame({'x':cell[0],'y':cell[1],'z':cell[2]})
-        # get a color dictionary
-        elements = lines[5].split()
-        atoms = lines[6].split()
-        hues = ['tab:blue','tab:orange','tab:green','tab:red']
-        colors = []
-        for i, atom in enumerate(atoms):
-            colors.extend([elements[i]]*int(atoms[i]))
+            struct_plot(self.struct)
 
-        zip_iterator = zip(elements,hues)
-        color_dict = dict(zip_iterator)
+    def _process_pick_file(self, event):
+        """ callback for menu """
+        grid = self.file_menu
+        with self.plotout:
+            clear_output(wait=True)
+            uploaded_file = grid.get_changed_df().at[grid.get_selected_rows()[0], 0] #not .at for metadata...
+            raw_string = codecs.decode(uploaded_file.value['POSCAR']['content']) #obtain index from selection...^
+            self.struct = self._induce_format()
+            struct_plot(self.struct)
 
-        size = 72
-        POSCAR_df = pd.DataFrame({'x':xyz[0],'y':xyz[1],'z':xyz[2],'element':colors,'size':size})    
-        fig = px.scatter_3d(POSCAR_df, x='x', y='y', z='z',
-                  color='element',labels={'x':'','y':'','z':''},size='size',size_max=size)
-
-        box = px.line_3d(box,x='x', y='y', z='z')
-        fig.add_traces(list(box.select_traces()))
-
-        fig.update_layout(scene = dict(
-                        xaxis = dict(
-                            nticks=0,showbackground=False,showticklabels=False,),
-                        yaxis = dict(
-                            nticks=0,showbackground=False,showticklabels=False,),
-                        zaxis = dict(
-                            nticks=0,showbackground=False,showticklabels=False,),),
-                        width=700,
-                        margin=dict(r=10, l=10, b=10, t=10)
-                      )
-        fig.show()
-
-class InputSuite():
+class SimSettingSuite():
     def __init__(self, inputs, struct_dict): #users_pp_file_list
 
         installedSimToolNotebooks = findInstalledSimToolNotebooks(simToolName,returnString=True)
